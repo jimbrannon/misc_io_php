1b423a82d4954efe26da6e11b550d7a665ec6f5d
Jim Brannon <jim.brannon@lrewater.com>
2012-06-26 17:00:22 -0600

    bunch of useful php io functions

---
+++ misc_io.php
@@ -0,0 +1,67 @@
+<?php
+function output_array($array){
+	foreach($array as $key => $val){
+		echo "    $key = ".$val."<br>";
+	}
+}
+function getargs ($key,$def) {
+	if(isset($_GET[$key])){
+		if(empty($_GET[$key])) {
+			$output = $def;
+		} else {
+			$output = $_GET[$key];
+		}
+	} else {
+		$output = $def;
+	}
+	return $output;
+}
+function do_offset($level){
+	$offset = "";             // offset for subarry
+	for ($i=1; $i<$level;$i++){
+		$offset = $offset . "<td></td>";
+	}
+	return $offset;
+}
+function show_array($array, $level, $sub){
+	if (is_array($array) == 1){          // check if input is an array
+		foreach($array as $key_val => $value) {
+			$offset = "";
+			if (is_array($value) == 1){   // array is multidimensional
+				echo "<tr>";
+				$offset = do_offset($level);
+				echo $offset . "<td>" . $key_val . "</td>";
+				show_array($value, $level+1, 1);
+			} else {                        // (sub)array is not multidim
+				if ($sub != 1){          // first entry for subarray
+					echo "<tr nosub>";
+					$offset = do_offset($level);
+				}
+				$sub = 0;
+				echo $offset . "<td main ".$sub." width=\"120\">" . $key_val .
+					"</td><td width=\"120\">" . $value . "</td>";
+				echo "</tr>\n";
+			}
+		} //foreach $array
+	} else { // argument $array is not an array
+		return;
+	}
+}
+function html_show_array($array){
+	echo "<table cellspacing=\"0\" border=\"2\">\n";
+	show_array($array, 1, 0);
+	echo "</table>\n";
+}
+function strtobool($str) {
+	switch (strtolower($str)) {
+		case "false":
+		case "no":
+		case "off":
+		case "0":
+			return false;
+			break;
+		default:
+			return true;
+	}
+}
+?>
